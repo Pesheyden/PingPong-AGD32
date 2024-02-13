ef5f7f8053201dadb3b333dfb902c1562e0c36ab
IvanS <Ivandmisem@gmail.com>
2024-02-13 22:35:19 +0200

    Everything about board, specifically: Board movement and control;  borders of board movement; class that stops boards

---
+++ Assets/Scripts/Board/RightBoardController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RightBoardController : MonoBehaviour
+{
+    [SerializeField] private float _boardSpeed;
+    [SerializeField] private float _borders;
+
+    private float ver;
+
+    private void FixedUpdate()
+    {
+        ver = Input.GetAxis("VerticalArrows");
+        if (((transform.position.y <= _borders && ver >= 0) ||
+            (transform.position.y >= -_borders && ver <= 0)) &&
+            !BoardPause.IsPaused)
+        {
+            Movement();
+        }
+    }
+    private void Movement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, transform.position + Vector3.up * ver, _boardSpeed);
+    }
+}
