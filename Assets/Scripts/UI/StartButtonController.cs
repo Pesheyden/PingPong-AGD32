f346c34ff0cbc23c90ba21e79e915a0bf5722cda
Марія Осіння <mariaosinnaa22@gmail.com>
2024-04-07 22:08:42 +0200

    Changed menu scene

---
+++ Assets/Scripts/UI/StartButtonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour
+{
+    [SerializeField] private AudioClip _soundClip;
+    private AudioSource _audioSource;
+
+    [SerializeField] private GameObject _fadeOut;
+
+    [SerializeField] private AudioSource _backgroundMusic;
+
+    void Start()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.clip = _soundClip;
+        _audioSource.playOnAwake = false;
+        _audioSource.volume = 0.4f;
+    }
+
+    public void PlaySound()
+    {
+        _audioSource.PlayOneShot(_soundClip);
+    }
+
+    public void FadeOut()
+    {
+        _fadeOut.SetActive(true);
+    }
+
+    public void StopBackgroundMusic()
+    {
+        _backgroundMusic.Stop();
+    }
+}
